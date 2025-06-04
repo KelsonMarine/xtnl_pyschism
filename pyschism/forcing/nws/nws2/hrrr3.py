wip commit, fixing writing of forcing files (time precision)

pyschism/forcing/nws/nws2/hrrr3.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 import pathlib
 import tempfile
 import logging
@@ -142,7 +142,7 @@ class AWSGrib2Inventory:
         grbfiles.sort()
         return grbfiles
 
-class HRRR:
+class HRRR: # TO DO !! make (SfluxDataset):
 
     def __init__(self, level=2, region='conus', bbox=None, pscr=None, outdir=None):
 
@@ -156,20 +156,22 @@ class HRRR:
     def write(self, start_date, rnday, air: bool=True, prc: bool=True, rad: bool=True):
 
         start_date = nearest_cycle() if start_date is None else start_date 
-        
-        if (start_date + timedelta(days=rnday)) > datetime.utcnow():
+        if type(rnday) == timedelta:
+            rnday = rnday.days
+
+        if (start_date + timedelta(days=rnday)) > datetime.now(timezone.utc):
             logger.info(f"End date is beyond current time, set rnday to one day and record to 2 days")
             rnday = 1
             self.record = 2 #days
 
-        end_date = start_date + timedelta(hours=rnday * self.record + 1)
+        end_date = start_date + timedelta(days=rnday * self.record)
         logger.info(f'start time is {start_date}, end time is {end_date}')
 
         if self.outdir is None:
             #self.outdir = pathlib.Path(start_date.strftime("%Y%m%d"))
             self.outdir = pathlib.Path("sflux")
-            self.outdir.mkdir(parents=True, exist_ok=True)
-        
+        self.outdir.mkdir(parents=True, exist_ok=True)
+
         datevector = np.arange(
             start_date, 
             start_date + timedelta(days=rnday), 
@@ -265,7 +267,7 @@ class HRRR:
         #write netcdf
         bdate = date.strftime('%Y %m %d %H').split(' ')
         bdate = [int(q) for q in bdate[:4]] + [0]
-
+        time_days = np.round((times - date.to_datetime64()) / np.timedelta64(1, 'D'), 5).astype('float64') # float32 was giving milisecond offsets in netcdf files, causing model to crash...
         if air:
             ds = xr.Dataset(
                 {
@@ -276,7 +278,7 @@ class HRRR:
                     'prmsl': (['time', 'ny_grid', 'nx_grid'], np.array(prmsl)),
                 },
                 coords = {
-                    'time': np.round((times - date.to_datetime64()) / np.timedelta64(1, 'D'), 5).astype('float32'),
+                    'time': time_days,
                     'lon': (['ny_grid', 'nx_grid'], lon),
                     'lat': (['ny_grid', 'nx_grid'], lat)
                 }
@@ -340,7 +342,7 @@ class HRRR:
                     'prate': (['time', 'ny_grid', 'nx_grid'], np.array(prate)),
                 },
                 coords = {
-                    'time': np.round((times - date.to_datetime64()) / np.timedelta64(1, 'D'), 5).astype('float32'),
+                    'time': time_days,
                     'lon': (['ny_grid', 'nx_grid'], lon),
                     'lat': (['ny_grid', 'nx_grid'], lat)
                 }
@@ -381,7 +383,7 @@ class HRRR:
                     'dswrf': (['time', 'ny_grid', 'nx_grid'], np.array(dswrf)),
                 },
                 coords = {
-                    'time': np.round((times - date.to_datetime64()) / np.timedelta64(1, 'D'), 5).astype('float32'),
+                    'time': time_days,
                     'lon': (['ny_grid', 'nx_grid'], lon),
                     'lat': (['ny_grid', 'nx_grid'], lat)
                 }

pyschism/forcing/wwm/base.py
@@ -76,12 +76,19 @@ class Parametric_Wave_Dataset(WWM):
     def dtype(self):
         return "Parametric_Wave_Dataset"
 
-    def write(self, path: Union[str, os.PathLike], overwrite: bool = True):
+    def write(
+            self, 
+            outdir: Union[str, os.PathLike], 
+            start_datetime: datetime = None,
+            rnday: timedelta = None,
+            end_datetime: datetime = None,
+            bbox = None,
+            overwrite: bool = True):
         """
         Implements the required write method.
         """
         
-        path = pathlib.Path(path)
+        path = pathlib.Path(outdir)
         path.mkdir(exist_ok=True)
         print(f"Writing Parametric_Wave_Dataset to {path}, overwrite={overwrite}")
 
@@ -96,13 +103,35 @@ class Parametric_Wave_Dataset(WWM):
             for var in ['dir','fp','hs','spr','t02']: # order of variables matters (or so people claim?)
                 filename = f"ww3_{var}.nc".strip()  # Ensure filename is clean.
                 f.write(filename + "\n") 
-        
+
+        # Subset ds based on optional inputs
+        if start_datetime is None and end_datetime is None and rnday is None:
+            ds = self.ds
+        else:
+            if start_datetime is None:
+                start_datetime = self.ds.time.isel(time=0).value
+            elif end_datetime is None and rnday is not None:
+                end_datetime = start_datetime + rnday
+
+            assert (end_datetime == start_datetime + rnday)
+
+            # Slice in time
+            ds = ds.sel(time=slice(start_datetime, end_datetime))
+
+        # Slice in space
+        if bbox is not None:
+            ds = ds.sel(
+                longitude=slice(bbox.xmin, bbox.xmax),
+                latitude=slice(bbox.ymin, bbox.ymax)
+            )
+
+        # write to netcdf
         for var in ['dir','fp','hs','spr','t02']: # order of variables matters (or so people claim?)
                 filename = f"ww3_{var}.nc".strip()  # Ensure filename is clean.
                 print(f'writing: {path / filename}')    
 
                 # extract variable
-                ds_var = self.ds[var]
+                ds_var = ds[var]
 
                 # apply valid min and valid max
                 valid_min = ds_var.attrs.get("valid_min", -np.inf)  # Default -inf if missing